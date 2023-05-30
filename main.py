2b45221e998b79edc145559ef7e10d333c8fa8c8
200411100061 - Diah Kamalia <101076428+diahkamalia@users.noreply.github.com>
2023-05-30 10:26:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -125,24 +125,6 @@ with st.container():
                 text = re.sub('nan', '', text)
 
                 return text
-#             st.info("## Text Cleaning")
-#             df['text'] = df['text'].apply(lambda x: cleaning(x))
-            
-#             st.info("## Tokenization")
-#             df['text_tokens'] = df['text'].apply(lambda x: word_tokenize(x))
-#             df[["text", "text_tokens"]].head()
-            
-#             st.info("## Stop Words Removal")
-#             stop_words = set(chain(stopwords.words('indonesian'), stopwords.words('english')))
-#             df['text_tokens'] = df['text_tokens'].apply(lambda x: [w for w in x if not w in stop_words])
-#             df[["text", "text_tokens"]].head(20)
-            
-#             st.info("## Stemming")
-#             tqdm.pandas()
-#             factory = StemmerFactory()
-#             stemmer = factory.create_stemmer()
-#             df['text_tokens'] = df['text_tokens'].progress_apply(lambda x: stemmer.stem(' '.join(x)).split(' '))
-#             df[["text", "text_tokens"]].head(20)
             
             st.info("## Cleaned Data")
             data = pd.read_csv('https://raw.githubusercontent.com/diahkamalia/DataMining1/main/cleanedtext.csv', index_col=0)
@@ -181,52 +163,7 @@ with st.container():
             y = data['Label'].values
             # y = data_vec.label.values
             X_train, X_test, y_train, y_test = train_test_split(X_pca, y ,test_size = 0.7, random_state =1)
-            # data['label'].value_counts()
-#             st.write("### Formula")
-#             st.latex(r'''
-#             X = \frac{X_i - X_{min}}{X_{max} - X_{min}}
-#             ''')
-#             st.warning("### Data Before Normalization")
-#             st.dataframe(df)
-#             label = df["Potability"]
-#             st.info("## Drop Column")
-#             st.write(" > Dropping 'Potability' Table")
-#             X = df.drop(columns=["Potability"])
-#             st.dataframe(X)
-#             st.info(" ## Normalisasi ")
-#             st.write(""" > ph, Hardness, Solids, Chloramines, Sulfate, Conductivity, Organic Carbon, Trihalomethanes, Turbidity """)
-#             st.write("#### Data After Normalization ")
-#             label=df["Potability"]
-#             data_for_minmax_scaler=pd.DataFrame(df, columns = ['ph', 'Hardness', 'Solids', 'Chloramines','Sulfate','Conductivity','Organic_carbon','Trihalomethanes','Turbidity'])
-#             data_for_minmax_scaler.to_numpy()
-#             scaler = MinMaxScaler()
-#             hasil_minmax=scaler.fit_transform(data_for_minmax_scaler)
-#             hasil_minmax = pd.DataFrame(hasil_minmax,columns = ['ph', 'Hardness', 'Solids', 'Chloramines','Sulfate','Conductivity','Organic_carbon','Trihalomethanes','Turbidity'])
-#             st.dataframe(hasil_minmax)
-#             X = hasil_minmax
-#             X=X.join(label) 
-#             st.success("## New Dataframe")
-#             st.dataframe(X)
-#             st.write("## Counting Data")
-#             st.write("- Drop Potability column from dataframe")
-#             st.write("- Split Data Training and Data Test")
-#             st.warning(""" ### Spliting Data""")
-#             st.write("""        
-#             - Data Training & X_train 
-#             - Data Test & X_test 
-#             - Data Training (Class) & y_train
-#             - Data Test (Class) & y_test
-#             """)
-            
-#             X   = hasil_minmax.iloc[:,:9]
-#             y  = df.iloc[:,-1]
-#             # membagi data menjadi set train dan test (70:30)
-#             X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=1,stratify=y)
-#             st.success("Showing X")
-#             st.write(X)
-            
-#             st.success("Showing Y")
-#             st.write(y)
+
 
         with classification : 
             st.write("""# Classification""")
@@ -247,88 +184,15 @@ with st.container():
             rf_eval_df = pd.DataFrame(rf_eval).transpose()
             st.header("Accuracy Result")
             st.info(f"Akurasi cita rasa dari Warung Amboina menggunakan metode Random Forest adalah : **{rf_accuracy}%** ")
-#             knn,gaussian,decision= st.tabs(["K-Nearest Neighbor", "Gaussian Naive Bayes", "Decision Tree"])
-#             with knn:
-#                 st.write("## K-Nearest Neighbor")
-#                 st.write( '<div style ="text-align: justify;" > Algortima K-Nearest Neighbor (KNN) adalah merupakan sebuah metode untuk melakukan klasifikasi terhadap obyek baru berdasarkan (K) tetangga terdekatnya. Metode pencarian jarak yang digunakan adalah Euclidean Distance yaitu perhitungan jarak terdekat. </div>', unsafe_allow_html = True)
-#                 st.write("### Formula")
-#                 st.latex(r'''
-#                 d(p,q) = d(p,q) = \sqrt{(q_1 - p_1)^2 + (q_2 - p_2)^2 + . . . + (q_n - p_n)^2}
-#                                 = \sqrt{\displaystyle\sum_{i=1}^n (q_i - p_i)^2}
-#                 ''')
-#                 st.write("### Step of the K-Nearest Neighbor Algorithm")
-#                 st.write("""
-#                 1. Menentukan parameter K (jumlah tetangga paling dekat)
-#                 2. Menghitung kuadrat jarak euclidian (euclidean distance) masing-masing obyek terhadap data sampel yang diberikan
-#                 3. Mengurutkan objek-objek tersebut ke dalam kelompok yang mempunyai jarak euclidean terkecil
-#                 4. Mengumpulkan kategori Y (klasifikasi nearest neighbor)
-#                 5. Dengan menggunakan kategori mayoritas,maka dapat diprediksikan nilai query instance yang telah dihitung
-#                 """)
-#                 # Inisialisasi K-NN
-#                 k_range = range(1,26)
-#                 scores = {}
-#                 scores_list = []
-#                 for k in k_range:
-#                     knn = KNeighborsClassifier(n_neighbors=k)
-#                     knn.fit(X_train, y_train)
-#                     y_pred_knn = knn.predict(X_test)
-#                     knn_accuracy = round(100 * accuracy_score(y_test, y_pred_knn), 2)
-#                     knn_eval = classification_report(y_test, y_pred_knn,output_dict = True)
-#                     knn_eval_df = pd.DataFrame(knn_eval).transpose()
-#                 st.header("Accuracy Result")
-#                 st.info(f"Water Potability accuracy of K-Nearest Neighbour model is : **{knn_accuracy}%** ")
-#                 st.write("> K = 1 - 25")
-            
-#             with gaussian:
-#                 st.write("## Gaussian Naive Bayes")
-#                 st.write(' <div style ="text-align: justify;"> Naive Bayes adalah algoritma machine learning untuk masalah klasifikasi. Ini didasarkan pada teorema probabilitas Bayes. Hal ini digunakan untuk klasifikasi teks yang melibatkan set data pelatihan dimensi tinggi. Beberapa contohnya adalah penyaringan spam, analisis sentimental, dan klasifikasi artikel berita.</div>', unsafe_allow_html = True)
-#                 st.write("### Formula")
-#                 st.latex(r'''
-#                 P(C_k | x) = \frac{P(C_k) P(x|C_k)}{P(x)}
-#                 ''')
-#                 # Inisialisasi Gaussian
-#                 gaussian    = GaussianNB()
-#                 gaussian.fit(X_train,y_train)
-#                 y_pred_gaussian   =  gaussian.predict(X_test)
-#                 gauss_accuracy  = round(100*accuracy_score(y_test, y_pred_gaussian),2)
-#                 gaussian_eval = classification_report(y_test, y_pred_gaussian,output_dict = True)
-#                 gaussian_eval_df = pd.DataFrame(gaussian_eval).transpose()
-#                 st.header("Accuracy Result")
-#                 st.info(f"Water Potability accuracy of Gaussian Naive Bayes model is : **{gauss_accuracy}%** ")
-                
-#             with decision:
-#                 st.write("## Decision Tree")
-#                 st.write('<div style ="text-align: justify;"> Decision tree merupakan alat pendukung keputusan dengan struktur seperti pohon yang memodelkan kemungkinan hasil, biaya sumber daya, utilitas, dan kemungkinan konsekuensi. Konsepnya adalah dengan cara menyajikan algoritma dengan pernyataan bersyarat yang meliputi cabang untuk mewakili langkah-langkah pengambilan keputusan yang dapat mengarah pada hasil yang menguntungkan.</div>' , unsafe_allow_html = True)
-#                 st.write("### Define Decision Tree Roots")
-#                 st.write("""
-#                 > Akar akan diambil dari atribut yang terpilih, dengan cara menghitung nilai gain dari masing – masing atribut. Nilai gain yang paling tinggi akan menjadi akar pertama. Sebelum menghitung niali gain dari atribut, harus menghitung nilai entropy terlebih dahulu
-#                 """)
-#                 st.write("### Formula Entropy")
-#                 st.latex(r'''
-#                 Entropy\left(\LARGE{D_1}\right) = - \displaystyle\sum_{i=1}^m p_i log_2 p_i
-#                 ''')
-#                 st.write("### Formula Gain (D1)")
-#                 st.latex(r'''
-#                 Gain(E_{new}) = E_{initial} - E_{new}
-#                 ''')
-#                 # Inisialisasi Decision Tree
-#                 decission3  = DecisionTreeClassifier(criterion="gini")
-#                 decission3.fit(X_train,y_train)
-#                 y_pred_decission3 = decission3.predict(X_test)
-#                 d3_accuracy = round(100*accuracy_score(y_test, y_pred_decission3),2)
-#                 d3_eval = classification_report(y_test, y_pred_decission3,output_dict = True)
-#                 d3_eval_df = pd.DataFrame(d3_eval).transpose()
-#                 st.header("Accuracy Result")
-#                 st.info(f"Water Potability accuracy of Decision Tree model is : **{d3_accuracy}%** ")
-                
+#
         with implementation:
             st.write("# Implementation")
             st.write("### Add Review :")
             text = st.text_input("Comment")
             result = st.button("Submit")
-                    input = [[Comment]]
-                    input['Comment'] = input['Comment'].apply(lambda x: cleaning(x))
-                    FIRST_IDX = 0
+            input = [[Comment]]
+            input['Comment'] = input['Comment'].apply(lambda x: cleaning(x))
+            FIRST_IDX = 0
 #                     if result:
 #                         use_model = knn
 #                         predictresult = use_model.predict(input_norm)[FIRST_IDX]
